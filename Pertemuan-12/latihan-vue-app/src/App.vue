606b2943aeb47e03b078bd3c7a046a97ec20cf76
naufal56-xtg <naufalseries56@gmail.com>
2023-06-14 11:31:03 +0700

    Latihan Vue JS

---
+++ Pertemuan-12/latihan-vue-app/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <NavbarComponent v-bind:nama="nama" item1="About" />
+  <h1>Hallo {{ nama }}</h1>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" class="hello" :mhs="mahasiswa" />
+  <input type="text" v-model="nama" class="mb-3">
+  <div class="d-flex justify-content-center">
+    <button @click="alert()" class="d-block">Alert</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NavbarComponent from "./components/NavbarComponent.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    NavbarComponent,
+  },
+  data() {
+    return {
+      nama: 'Naufal Hadi',
+      mahasiswa: [
+        {
+          nama: 'Bambang Susanto',
+          usia: 18,
+          jk: 'Laki - Laki'
+        },
+        {
+          nama: 'Santi Susanti',
+          usia: 19,
+          jk: 'Perempuan'
+        },
+        {
+          nama: 'Asep Nurohman',
+          usia: 20,
+          jk: 'Laki - Laki'
+        },
+        {
+          nama: 'Rizki Muhammad',
+          usia: 18,
+          jk: 'Laki - Laki'
+        },
+      ]
+    }
+  },
+  methods: {
+    alert() {
+      alert('Hallo ' + this.nama)
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: aqua;
+}
+
+.hello {
+  margin-bottom: 20px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
---
+++ Pertemuan-12/latihan-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>Latihan Vue JS</h3>
+  </div>
+
+  <div class="d-flex justify-content-center">
+    <div class="col-md-4">
+      <table class="table table-bordered">
+      <thead>
+        <tr>
+          <td>No</td>
+          <td>Nama</td>
+          <td>Umur</td>
+          <td>Jenis Kelamin</td>
+        </tr>
+      </thead>
+      <tbody v-for="(data, index) in mhs" :key="index">
+        <tr>
+          <td>{{ index + 1 }}</td>
+          <td>{{ data.nama }}</td>
+          <td>{{ data.usia }}</td>
+          <td>{{ data.jk }}</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    mhs: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: aqua;
+}
+h3 {
+  margin: 40px 0 0;
+  color: aqua;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
